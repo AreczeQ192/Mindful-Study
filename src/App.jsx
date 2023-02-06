e2c0e9a877644c716f8448b511d64947d71d7fc8
AreczeQ192 <97028504+AreczeQ192@users.noreply.github.com>
2023-02-06 17:12:30 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 import './App.css'
-import Todo from './components/Todo'
-import Calendar from './components/Calendar'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-
+import Application from './components/Application'
 
 function App() {
   return (
-    <>
-    <Navbar />
-    {/* <Todo /> */}
-    <Calendar />
-    <Footer />
-    </> 
+    <Application />
   )
 }
 
