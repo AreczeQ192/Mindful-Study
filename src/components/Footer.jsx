ba8b05d3ec7c5da054bc5adf118a15801759c7ba
AreczeQ192 <97028504+AreczeQ192@users.noreply.github.com>
2023-02-06 12:46:47 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/Main.css'
+
+export default function Footer() {
+  return (
+    <footer>
+        <button className='footButton'>1</button>
+        <button className='footButton'>2</button>
+        <button className='footButton'>3</button>
+        <button className='footButton'>4</button>
+        <button className='footButton'>5</button>
+    </footer>
+  )
+}
