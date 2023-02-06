dcdd96c6fc5ac641e9aa508558e7ef3adfc4743a
AreczeQ192 <97028504+AreczeQ192@users.noreply.github.com>
2023-02-06 12:42:24 +0100

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../css/Main.css'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <button className='navButton'></button>
+    </nav>
+  )
+}
